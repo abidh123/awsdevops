f4524d52aaa5b15608173d3aeab0e5e6f9470150
Abidh <sabidh@deloitte.com>
2023-06-02 18:29:41 +0530

    new changes

--- aks-purpose-project/server.js
+++ aks-purpose-project/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World sssss');
+  res.send('Hello World one');
 });
 
 app.listen(PORT, HOST, () => {
