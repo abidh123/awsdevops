5ceebec93705b235d2aa51321f248a3baa561050
abidh123 <57485223+abidh123@users.noreply.github.com>
2023-07-28 10:50:09 +0530

    Update server.js

--- aks-purpose-project/server.js
+++ aks-purpose-project/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World feature branch changed');
+  res.send('Hello World feature branch changes');
 });
 
 app.listen(PORT, HOST, () => {
